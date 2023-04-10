1a67db26179e421962ad7dc2fd43466b7b0e23bc
ArtemYemtsev <tema2201@gmail.com>
2023-04-10 13:01:04 +0300

    Home work 4 is done

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-alert('Hello, '+prompt('What is your name?')+'! How are you?')
+let a = Number(prompt(`Enter first number.`));
+let b = Number (prompt(`Enter second number.`));
+alert(`${a}+${b}=${a+b}\n${a}-${b}=${a-b}\n${a}*${b}=${a*b}\n${a}/${b}=${a/b}`);
