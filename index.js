ec3013744030f0c717f8e6ed4d567e336326e8b6
ArtemYemtsev <tema2201@gmail.com>
2023-04-10 22:10:28 +0300

    Home work 3 is done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-
+let firstNumber = Number(prompt(`Enter first number.`));
+let secondNumber = Number(prompt(`Enter second number.`));
+let thirdNumber = Number(prompt(`Enter third number.`));
+let average = (firstNumber+secondNumber+thirdNumber)/3;
+alert(`Average of numbers = ${average}`);
