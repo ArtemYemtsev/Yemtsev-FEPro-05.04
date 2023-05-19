f69f2443c911ea6c59c3d39dab86f1a019c86d9d
ArtemYemtsev <tema2201@gmail.com>
2023-05-19 13:24:37 +0300

    hw19 is done

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 let img = document.createElement('img')
+let random = () => {
+    return Math.ceil(Math.random() * 9)
+}
 
-let rand = Math.ceil(Math.random() * 9)
-img.setAttribute('src',`images/${rand}.jpg`)
-
+img.src = `images/${random()}.jpg`
 document.body.append(img)
 
-
+setInterval(()=>{
+    img.setAttribute('src',`images/${random()}.jpg`)
+    }
+    ,5000)
 
 
 
