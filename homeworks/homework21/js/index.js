e3a1249c5eecbf6d7b116533fd79f704b74e2676
ArtemYemtsev <tema2201@gmail.com>
2023-05-28 17:09:13 +0300

    hw21 is done

--- homeworks/homework21/js/index.js
+++ homeworks/homework21/js/index.js
@@ -3,52 +3,114 @@ let offers = [
         id: '001',
         category: 'cars',
         image: '001.webp',
-        title: 'Экскаватор колёсный 1',
+        title: 'Экскаватор колёсный',
         description: 'Экскаватор из серии Lena Truxx. Подвижный ковш управляется с помощью ручки. Стальные оси и открывающаяся кабина. В комплекте фигурка рабочего.',
         price: '1080'
     },
     {
         id: '002',
+        category: 'cars',
+        image: 'ford01.webp',
+        title: 'Автомодель Bburago Ford Shelby GT500',
+        description: 'Игровая ценность модели в том, что она имеет высокий уровень детализации, что обеспечивает максимальное сходство с оригиналом.',
+        price: '445'
+    },
+    {
+        id: '003',
+        category: 'cars',
+        image: 'hw01.jpg',
+        title: 'МАШИНА HOT WHEELS БАЗОВАЯ 10 ШТ',
+        description: 'Набір Hot Wheels. Неймовірні гонки з швидкими машинками, віражі та небезпечні пастки на треках.',
+        price: '799'
+    },
+    {
+        id: '004',
         category: 'dolls',
         image: 'barby.webp',
-        title: 'Барби 1',
+        title: 'Барби',
         description: 'С куклой Барби Королевская невеста каждая девочка сможет провести репетицию собственной свадьбы у себя дома.',
         price: '1000'
     },
     {
-        id: '003',
+        id: '005',
+        category: 'dolls',
+        image: 'doll02.jpg',
+        title: 'Кукла LORI 15 см Сабелла',
+        description: 'Кукла LORI Сабелла - пополнит коллекцию друзей LORI!',
+        price: '369'
+    },
+    {
+        id: '006',
+        category: 'dolls',
+        image: 'grogu.jpg',
+        title: 'Фигурка Hasbro Star Wars Мандалорец Грогу',
+        description: 'Фигурка Hasbro Star Wars Малыш Йода Мандалорец порадует любителей вселенной «Звездных войн» и дополнит их коллекцию героев.',
+        price: '199'
+    },
+    {
+        id: '007',
         category: 'books',
         image: 'spokoynoy-nochi-kotenok.png',
-        title: 'Спокойной ночи, котёнок 1',
+        title: 'Спокойной ночи, котёнок',
         description: 'Котенок уже большой. Но сегодня он никак не может заснуть. Хорошо, что у него есть друзья, они всегда готовы прийти на помощь.',
         price: '150'
+    },
+    {
+        id: '008',
+        category: 'books',
+        image: 'slon01.jpg',
+        title: 'Такой большой слонёнок',
+        description: 'Это история о слонёнке, который впервые отправился гулять без мамы.',
+        price: '160'
+    },
+    {
+        id: '009',
+        category: 'books',
+        image: 'zaec01.png',
+        title: 'Спокойной ночи, зайчонок!',
+        description: 'Это книга-игра, которую можно читать вечером, чтобы уложить малыша спать. Вместе с главным героем книжки, зайчонком, он пройдет все этапы подготовки ко сну.',
+        price: '174'
     }
 ]
 
 let offerBuffer = []
 
+let categoryList = document.getElementById('category')
+categoryList.addEventListener('click', (event) => {
+    clear()
+    clearCategory()
+
+    if (event.target.tagName === 'LI'){
+        event.target.classList.add('active')
+        getOffer(event.target.id)
+        renderCards(offerBuffer)
+    }
+})
+
+function clearCategory(){
+    for (let item of categoryList.querySelectorAll('li')) {
+        if (item.classList.contains('active')){
+            item.classList.remove('active')
+        }
+    }
+}
+
 function getOffer(category){
-    offerBuffer = offers.filter((item) =>{
+    return offerBuffer = offers.filter((item) =>{
         if (item.category === category || category === 'all'){
             return true
         } else return false
     })
-    return offerBuffer
 }
 
 function renderCards (arr){
     let cardList = document.getElementById('card-list')
-    
-    for (let item of cardList.querySelectorAll('LI')){
-        if (item.classList.contains('display')){
-            item.remove()
-        }
-    }
+    clearCards()
 
     for (let i = 0; i < arr.length; i++){
         let card = document.getElementById('card').cloneNode(true)
         card.setAttribute('id',`${arr[i].id}`)
-        card.querySelector('img').setAttribute('src',`images/${arr[i].image}`)
+        card.querySelector('img').setAttribute('src',`images/product/${arr[i].image}`)
         card.querySelector('h3').innerHTML = arr[i].title
         card.querySelector('span').innerHTML = arr[i].price
         card.classList.add('display')
@@ -56,47 +118,44 @@ function renderCards (arr){
     }
 }
 
-function showInfo(id){
-    let cardInfo = document.getElementById('card-info')
-    let product = {}
-    for (let item of offerBuffer){
-        if (item.id === id){
-            product = item
+function clearCards(){
+    for (let item of cardList.querySelectorAll('LI')){
+        if (item.classList.contains('display')){
+            item.remove()
         }
     }
-
-    cardInfo.querySelector('h3').innerHTML = product.title
-    cardInfo.querySelector('p').innerHTML = product.description
-    cardInfo.querySelector('span').innerHTML = product.price
-    cardInfo.classList.add('display')
-    
 }
 
-let categoryList = document.getElementById('category')
-categoryList.addEventListener('click', (event) => {
-
-    for (let item of categoryList.querySelectorAll('li')) {
-        if (item.classList.contains('active')){
-            item.classList.remove('active')
+let cardInfo = document.getElementById('card-info')
+function showInfo(id){
+    for (let item of offerBuffer){
+        if (item.id === id){
+            cardInfo.querySelector('h3').innerHTML = item.title
+            cardInfo.querySelector('p').innerHTML = item.description
+            cardInfo.querySelector('span').innerHTML = item.price
+            cardInfo.classList.add('display')
         }
     }
-
-    if (event.target.tagName === 'LI'){
-        event.target.classList.add('active')
-        getOffer(event.target.id)
-        renderCards(offerBuffer)
-    }
-
-})
+}
 
 let cardList = document.getElementById('card-list')
 cardList.addEventListener('click', (event) => {
-    console.log(event)
-        if (event.target.tagName === 'LI' || event.target.children['*']){
+    console.log(event.target)
+        if (event.target.tagName === 'LI'){
             showInfo(event.target.id)
-            console.log(event.target)
+        } else if (event.target.tagName === 'IMG' || event.target.tagName === 'H3' || event.target.tagName === 'P'){
+            showInfo(event.srcElement.parentElement.id)
         }
-        
-    
 })
 
+const btnBy = document.getElementById('btnBy')
+btnBy.addEventListener('click', ()=>{
+    alert('Товар куплен')
+    clear()
+})
+
+function clear(){
+    clearCards()
+    clearCategory()
+    cardInfo.classList.remove('display')
+}
