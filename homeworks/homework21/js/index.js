55a3441111ddf05a3e3e90935eeedd5b08fa11b2
ArtemYemtsev <tema2201@gmail.com>
2023-06-01 11:10:08 +0300

    hw21 fixed code, edited card generation

--- homeworks/homework21/js/index.js
+++ homeworks/homework21/js/index.js
@@ -73,34 +73,76 @@ let offers = [
     }
 ]
 
+let cat = [
+    {
+        id: 'cat001',
+        name: 'cars',
+        title: 'Машинки'
+    },
+    {
+        id: 'cat002',
+        name: 'dolls',
+        title: 'Куклы'
+    },
+    {
+        id: 'cat003',
+        name: 'books',
+        title: 'Книги'
+    },
+    {
+        id: 'cat000',
+        name: 'all',
+        title: 'Все'
+    }
+]
+
+const classActive = 'active'
+const classDisplay = 'display'
+const classCatListItem = 'shop__form__category__list__item'
+const classCardItem = 'shop__form__cards__item'
+const classCardImg = 'card__image'
+const classCardTitle = 'card__title'
+const classCardPrice = 'card__price'
+const classCardPriceValue = 'price'
+
 let offerBuffer = []
+
+const renderCategories = (cat) => {
+    let catList = document.getElementById('category')
+
+    for (let i = 0; i < cat.length; i++){
+        let catItem = document.createElement('li')
+        catItem.classList.add(classCatListItem)
+        catItem.id = `${cat[i].name}`
+        catItem.innerHTML = `${cat[i].title}`
+        catList.appendChild(catItem)
+    }
+}
+
+renderCategories(cat)
 
 let categoryList = document.getElementById('category')
 categoryList.addEventListener('click', (event) => {
     clear()
     clearCategory()
 
-    if (event.target.tagName === 'LI'){
-        event.target.classList.add('active')
+    if (event.target.classList.contains(classCatListItem)){
+        event.target.classList.add(classActive)
         getOffer(event.target.id)
         renderCards(offerBuffer)
     }
 })
 
 function clearCategory(){
-    for (let item of categoryList.querySelectorAll('li')) {
-        if (item.classList.contains('active')){
-            item.classList.remove('active')
+    for (let item of categoryList.querySelectorAll(`.${classCatListItem}`)) {
+        if (item.classList.contains(classActive)){
+            item.classList.remove(classActive)
         }
     }
 }
 
 function getOffer(category){
-    return offerBuffer = offers.filter((item) =>{
-        if (item.category === category || category === 'all'){
-            return true
-        } else return false
-    })
+    return offerBuffer = offers.filter((item) => (item.category === category || category === 'all'))
 }
 
 function renderCards (arr){
@@ -108,19 +150,36 @@ function renderCards (arr){
     clearCards()
 
     for (let i = 0; i < arr.length; i++){
-        let card = document.getElementById('card').cloneNode(true)
+        let card = createCard()
         card.setAttribute('id',`${arr[i].id}`)
-        card.querySelector('img').setAttribute('src',`images/product/${arr[i].image}`)
-        card.querySelector('h3').innerHTML = arr[i].title
-        card.querySelector('span').innerHTML = arr[i].price
-        card.classList.add('display')
+        card.querySelector(`.${classCardImg}`).setAttribute('src',`images/product/${arr[i].image}`)
+        card.querySelector(`.${classCardTitle}`).innerHTML = arr[i].title
+        card.querySelector(`.${classCardPriceValue}`).textContent = arr[i].price
+        card.classList.add(classDisplay)
         cardList.appendChild(card)
     }
 }
 
+const createCard = () => {
+    let card = document.createElement('li')
+    card.classList.add(classCardItem)
+    const cardTags = ['img','h3','p','span']
+    for (let item of cardTags){
+        if (item == 'span'){
+            card.querySelector('p').appendChild(document.createElement(item))
+            card.querySelector('p').appendChild(document.createTextNode(String.fromCharCode(0x20B4)))
+        } else card.appendChild(document.createElement(item))
+    }
+    card.querySelector('img').classList.add(classCardImg)
+    card.querySelector('h3').classList.add(classCardTitle)
+    card.querySelector('p').classList.add(classCardPrice)
+    card.querySelector('span').classList.add(classCardPriceValue)
+    return card
+}
+
 function clearCards(){
-    for (let item of cardList.querySelectorAll('LI')){
-        if (item.classList.contains('display')){
+    for (let item of cardList.querySelectorAll(`.${classCardItem}`)){
+        if (item.classList.contains(classDisplay)){
             item.remove()
         }
     }
@@ -130,22 +189,25 @@ let cardInfo = document.getElementById('card-info')
 function showInfo(id){
     for (let item of offerBuffer){
         if (item.id === id){
-            cardInfo.querySelector('h3').innerHTML = item.title
-            cardInfo.querySelector('p').innerHTML = item.description
-            cardInfo.querySelector('span').innerHTML = item.price
-            cardInfo.classList.add('display')
+            cardInfo.querySelector('.card-info__title').innerHTML = item.title
+            cardInfo.querySelector('.card-info__desription').innerHTML = item.description
+            cardInfo.querySelector(`.${classCardPriceValue}`).innerHTML = item.price
+            cardInfo.classList.add(classDisplay)
         }
     }
 }
 
 let cardList = document.getElementById('card-list')
 cardList.addEventListener('click', (event) => {
-    console.log(event.target)
-        if (event.target.tagName === 'LI'){
-            showInfo(event.target.id)
-        } else if (event.target.tagName === 'IMG' || event.target.tagName === 'H3' || event.target.tagName === 'P'){
-            showInfo(event.srcElement.parentElement.id)
-        }
+    
+    const etc = event.target.classList
+    const checkCardTags = etc.contains(classCardImg) || etc.contains(classCardTitle) || etc.contains(classCardPrice)
+    
+    if (etc.contains(classCardItem)){
+        showInfo(event.target.id)
+    } else if (checkCardTags){
+        showInfo(event.srcElement.parentElement.id)
+    }
 })
 
 const btnBy = document.getElementById('btnBy')
@@ -157,5 +219,5 @@ btnBy.addEventListener('click', ()=>{
 function clear(){
     clearCards()
     clearCategory()
-    cardInfo.classList.remove('display')
+    cardInfo.classList.remove(classDisplay)
 }
